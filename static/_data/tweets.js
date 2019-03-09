e19f5f82707785eeff6b8c46c82f1c773390ce97
mattdsteele <orphum@gmail.com>
2019-03-09 15:46:20 -0600

    Lazy load images, why not

--- static/_data/tweets.js
+++ static/_data/tweets.js
@@ -5,7 +5,7 @@ module.exports = async () => {
   });
   const query = store.createQuery('Tweet-firedhuskers').order('Date');
   const [res, more] = await store.runQuery(query);
-  console.log('more', res.length);
+  console.log('more', Object.keys(res[0]));
   console.log(more.moreResults);
   return res;
 };
